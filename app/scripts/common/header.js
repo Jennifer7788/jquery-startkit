3aaa303e186bbaed8954a67128f7ec4293367654
chenfenqiang <429365799@qq.com>
2019-05-28 23:52:32 +0800

    添加动画

--- app/scripts/common/header.js
+++ app/scripts/common/header.js
@@ -5,15 +5,28 @@
      * 初始化头部滚动事件
      */
     eventHandlers() {
+      // 滚动事件
       $(document.body).scroll(e => {
         this.headerBehavior(e.target.scrollTop, e.target.scrollLeft);
       });
 
+      // 点击menu item事件
+      $('#menu li').click(function (e) {
+        $('#menu li').removeClass('check');
+        $(this).addClass('check');
+      });
     },
     // 头部动画
     headerBehavior(scrollTop, scrollLeft) {
       // header横向滚动同步
       $('#pageHeader').css('transform', `translateX(-${scrollLeft}px)`);
+
+      // 滚动的时候缩小header的高度
+      if (scrollTop > 0) {
+        $('#pageHeader').addClass('mini');
+      } else {
+        $('#pageHeader').removeClass('mini');
+      }
     },
   }
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,79 +1,63 @@
 !(($) => {
   const IndexPage = {
-    // 是否在加载更多数据中
-    loadingMore: false,
+
 
     init() {
       this.initEvents()
     },
 
     initEvents() {
-      // 距离底部多少的时候触发加载更多
-      const TO_BOTTOM_DISTANCE = 30;
+      $(document.body).scroll(e => {
+        // 业务模块滚动动画
+        this.businessSectorAnimation();
 
-      $('#indexContainer').scroll(e => {
-        if (this.loadingMore) {
-          return false;
-        }
+        // 核心优势滚动动画
+        this.businessModuleAnimation('#brandPremiun');
+        this.businessModuleAnimation('#withAbundantCapital');
 
-        // 判断是否滚动到底部: 页面已经滚动到上面的长度 + 页面显示中的长度 > 页面的总长度 - 距离底部多少距离
-        // 事实上，页面的总长度 = 页面已经滚动到屏幕上面的长度 + 页面显示中的长度
-        // 这个 “距离底部多少距离” 意思是页面滚动距离底部还有30像素的时候就认为滚动到底部
-        if (e.target.scrollTop + e.target.clientHeight > e.target.scrollHeight - TO_BOTTOM_DISTANCE) {
-          this.loadingMore = true;
-
-          // 延迟一秒模拟异步请求
+        // -------------------------
+        let $professionalManagement = $('#professionalManagement')[0].getBoundingClientRect();
+        if ($(document.body).innerHeight() - $professionalManagement.top > $professionalManagement.height / 3) {
+          $('#professionalManagement .bg-img1').addClass('active');
           setTimeout(() => {
-            const itemContent = [...new Array(10)].map((_, index) => {
-              if (index % 2 === 0) {
-                return `
-                  <a href="./pages/article.html" class="list-item">
-                    <div class="item-left">
-                      <div class="item-img"
-                        style="background-image: url(https://www.volanews.com/upload/portal/20190520/d01310d5be9b51a83f5a84961fd137c0.jpg)">
-                      </div>
-                    </div>
-                    <div class="item-right">
-                      <div class="item-title">Smartphone companies launched a price war in India</div>
-                      <div class="item-bottom">
-                        <span>by Olivia</span>
-                        <span>
-                          <img src="./images/date.png" width="10">
-                          <i class="time">7 hours ago</i>
-                        </span>
-                      </div>
-                    </div>
-                  </a>
-                `
-              } else {
-                return `
-                  <a href="./pages/article.html" class="list-item">
-                    <div class="item-left">
-                      <div class="item-img"
-                        style="background-image: url(https://www.volanews.com/upload/portal/20190520/eb6ee8fa731970078f54379da12ae525.png)">
-                      </div>
-                    </div>
-                    <div class="item-right">
-                      <div class="item-title">2019 Fintech Trends to Watch</div>
-                      <div class="item-bottom">
-                        <span>by Kyle</span>
-                        <span>
-                          <img src="./images/date.png" width="10">
-                          <i class="time">7 hours ago </i>
-                        </span>
-                      </div>
-                    </div>
-                  </a>
-                `
-              }
-            })
-
-            $('#smallList').append(itemContent);
-            
-            this.loadingMore = false;
-          }, 1000);
+            $('#professionalManagement .text-area').addClass('active');
+            setTimeout(() => {
+              $('#professionalManagement .title').addClass('active');
+              $('#professionalManagement .horizontal-line').addClass('active');
+              setTimeout(() => {
+                $('#professionalManagement .EN-text-area').addClass('active');
+              }, 300);
+            }, 300);
+          }, 200);
+        } else {
+          $('#professionalManagement .title').removeClass('active');
+          $('#professionalManagement .text-area').removeClass('active');
+          $('#professionalManagement .bg-img1').removeClass('active');
+          $('#professionalManagement .horizontal-line').removeClass('active');
+          $('#professionalManagement .EN-text-area').removeClass('active');
         }
-      })
+      });
+    },
+
+    // 核心优势动画通用封装
+    businessModuleAnimation(targetId) {
+      let $dom = $(targetId)[0].getBoundingClientRect();
+      if ($(document.body).innerHeight() - $dom.top > $dom.height / 3) {
+        $(`${targetId} .row-content`).addClass('active');
+      } else {
+        $(`${targetId} .row-content`).removeClass('active');
+      }
+    },
+
+    // 业务模块动画通用封装
+    businessSectorAnimation() {
+      let $businessSector = $('#businessSector')[0].getBoundingClientRect();
+
+      if ($(document.body).innerHeight() - $businessSector.top > $businessSector.height / 3) {
+        $('#businessModule .img-con').addClass('active');
+      } else {
+        $('#businessModule .img-con').removeClass('active');
+      }
     }
   }
 
