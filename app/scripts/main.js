f37bd28ebf204cd104db666d803e699a76f33b32
xieyanna <785061676@qq.com>
2019-05-29 18:51:24 +0800

    动画完善

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,21 +14,23 @@
         // 核心优势滚动动画
         this.businessModuleAnimation('#brandPremiun');
         this.businessModuleAnimation('#withAbundantCapital');
+        this.businessModuleAnimation('#professionalManagement');
         this.businessModuleAnimation('#highQualityProducts');
         this.businessModuleAnimation('#industrialSupporting');
-
-        // -------------------------
-        let $professionalManagement = $('#professionalManagement')[0].getBoundingClientRect();
-        if ($(document.body).innerHeight() - $professionalManagement.top > $professionalManagement.height / 3) {
-          $('#professionalManagement').addClass('active');
-        } else {
-          $('#professionalManagement').removeClass('active');
-        }
       });
     },
 
     // 核心优势动画通用封装
-    businessModuleAnimation(targetId) {
+     businessModuleAnimation(targetId) {
+       // -------------------------
+       let $professionalManagement = $(targetId)[0].getBoundingClientRect();
+       if ($(document.body).innerHeight() - $professionalManagement.top > $professionalManagement.height / 3) {
+         $(targetId).addClass('active');
+       } else {
+         $(targetId).removeClass('active');
+       }
+    },
+    businessModuleAnimation1(targetId) {
       let $dom = $(targetId)[0].getBoundingClientRect();
       if ($(document.body).innerHeight() - $dom.top > $dom.height / 3) {
         $(`${targetId} .row-content`).addClass('active');
@@ -47,6 +49,15 @@
         $('#businessModule .img-con').removeClass('active');
       }
     }
+   /* businessSectorAnimation() {
+      let $businessSector = $('#businessSector')[0].getBoundingClientRect();
+
+      if ($(document.body).innerHeight() - $businessSector.top > $businessSector.height / 3) {
+        $('#businessModule .img-con').addClass('active');
+      } else {
+        $('#businessModule .img-con').removeClass('active');
+      }
+    }*/
   }
 
   IndexPage.init()
